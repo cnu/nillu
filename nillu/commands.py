d94378980a70579f85c968581b52a5337d887268
Srinivasan R <srinivasanr@gmail.com>
2017-04-13 21:36:53 +0530

    add a list_routes flask command
    This prints all the routes available in the flask app.

--- nillu/commands.py
+++ nillu/commands.py
@@ -9,3 +9,19 @@ def initdb_command():
     """Creates the database tables."""
     init_db()
     print('Initialized the database.')
+
+
+@app.cli.command('list_routes')
+def list_routes():
+    import urllib.parse
+    output = []
+    for rule in app.url_map.iter_rules():
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+        methods = ','.join(rule.methods)
+        line = urllib.parse.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, rule))
+        output.append(line)
+
+    for line in sorted(output):
+        print(line)
