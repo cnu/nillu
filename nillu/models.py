d7cd01c2d8a80f2f91cc783408c7552e0e62a81e
Srinivasan Rangarajan <srinivasanr@gmail.com>
2017-04-14 12:51:27 +0530

    add a password column to User table
    fixed #9

--- nillu/models.py
+++ nillu/models.py
@@ -8,11 +8,14 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
     role = db.Column(db.Enum('developer', 'non-developer'))
 
-    def __init__(self, name=None, email=None):
+    def __init__(self, name, password, email, role):
         self.name = name
+        self.password = password
         self.email = email
+        self.role = role
 
     def __repr__(self):
         return '<User {}>'.format(self.name)
