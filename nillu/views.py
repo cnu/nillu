0e7ef4156df3e690b35a1f6806c62cfa69c6dd85
Srinivasan Rangarajan <srinivasanr@gmail.com>
2017-04-15 00:17:53 +0530

    Write parser for the date param in entries view function
    handle different types of date formats like "2017" or "2017/04" or "2017/04/14"
    also handle "today" and "yesterday" for easy access.

--- nillu/views.py
+++ nillu/views.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import flash, request, abort, redirect, render_template, url_for
 from flask_login import login_user, login_required, logout_user
 
@@ -11,6 +12,54 @@ from nillu.utils import is_safe_url
 @login_required
 def index():
     return 'Hello World!'
+
+
+@app.route('/entries/<path:date>/')
+@login_required
+def entries(date):
+    """Display the entries for a particular date
+
+    :param date: the date for which to get the entries
+
+    TODO:
+    date could be just an year, year/month or year/month/date
+    Eg: "2017" or "2017/04" or "2017/04/14"
+    Based on which format is used, the corresponding entries for the
+    time period is fetched and displayed.
+    """
+    today = datetime.date.today()
+    try:
+        if date.lower() == 'today':
+            date_obj = today
+            resolution = 'day'
+        elif date.lower() == 'yesterday':
+            date_obj = today - datetime.timedelta(days=1)
+            resolution = 'day'
+        else:
+            slash_count = date.count('/')
+            if slash_count == 2:
+                # year/month/date format
+                date_obj = datetime.datetime.strptime(date, '%Y/%m/%d').date()
+                resolution = 'day'
+            elif slash_count == 1:
+                # year/month format
+                date_obj = datetime.datetime.strptime(date, '%Y/%m').date()
+                resolution = 'month'
+            elif slash_count == 0:
+                # year format
+                date_obj = datetime.datetime.strptime(date, '%Y').date()
+                resolution = 'year'
+            else:
+                # fallback to today
+                date_obj = today
+                resolution = 'day'
+
+            if date_obj - today > 0:
+                flash("Great Scott! Your flux capacitor is broken.", 'warning')
+                return redirect(url_for('entries', date='today'))
+    except ValueError:
+        flash("Couldn't get entries for specified date.", 'danger')
+        return redirect(url_for('entries', date='today'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
