a6a471b4cf3e429b8d32c71db96f1f0dfc9f8fd6
Srinivasan Rangarajan <srinivasanr@gmail.com>
2017-04-15 10:53:40 +0530

    Test correct and incorrect login views
    fixes #28

--- tests/test_login.py
+++ tests/test_login.py
@@ -8,6 +8,13 @@ class LoginTestCase(BaseTestCase):
         rv = self.client.get('/')
         self.assertRedirects(rv, '/login?next=%2F')
 
+    def test_correct_login(self):
+        rv = self.login('foo@example.com', 'foo')
+        assert b'Logged in Successfully.' in rv.data
+
+    def test_incorrect_login(self):
+        rv = self.login('foo@example.com', 'foo1')
+        assert b'Wrong email/password.' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
