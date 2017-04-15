f51254415f9a97898fc6535a995247b991217d1c
Srinivasan Rangarajan <srinivasanr@gmail.com>
2017-04-15 08:26:25 +0530

    redirect to login when accessing a URL without authenticating oneself
    Also display a warning message instead of plain flash message.
    fixes #24

--- nillu/__init__.py
+++ nillu/__init__.py
@@ -6,6 +6,9 @@ app.config.from_pyfile('settings.py')
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "warning"
+
 
 import nillu.views
 import nillu.commands
