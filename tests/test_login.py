3ee5a164f3e36a8bab3b4ac669d286c0cc6b599d
Srinivasan Rangarajan <srinivasanr@gmail.com>
2017-04-15 10:52:08 +0530

    rename response variable to rv

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,8 +5,8 @@ from tests import BaseTestCase
 
 class LoginTestCase(BaseTestCase):
     def test_non_login_redirect(self):
-        r = self.client.get('/')
-        self.assertRedirects(r, '/login?next=%2F')
+        rv = self.client.get('/')
+        self.assertRedirects(rv, '/login?next=%2F')
 
 
 if __name__ == '__main__':
