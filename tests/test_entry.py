8863929cf2deec45a2bb0a716d607ea456e6a5b3
Srinivasan Rangarajan <srinivasanr@gmail.com>
2017-04-16 00:56:38 +0530

    Write test case for basic /entry/today/ page

---
+++ tests/test_entry.py
@@ -0,0 +1,19 @@
+import unittest
+
+from tests import BaseTestCase
+
+
+class EntryTestCase(BaseTestCase):
+    def test_entry_today(self):
+        rv = self.login('foo@example.com', 'foo')
+        rv = self.client.get('/entry/today/')
+        self.assertIn(b'user 1 entry - done', rv.data)
+        self.assertIn(b'user 1 entry - todo', rv.data)
+        self.assertIn(b'user 1 entry - blocking', rv.data)
+        self.assertIn(b'user 2 entry - done', rv.data)
+        self.assertIn(b'user 2 entry - todo', rv.data)
+        self.assertIn(b'user 2 entry - blocking', rv.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
