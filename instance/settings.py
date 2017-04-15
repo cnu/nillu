34d271e8f291a8b12098e050199953fd56576fb7
Srinivasan Rangarajan <srinivasanr@gmail.com>
2017-04-15 21:05:41 +0530

    Use a postgresql DB for dev instead of sqlite
    anyway it is going to be deployed on a PG machine. So develop on PG.

--- instance/settings.py
+++ instance/settings.py
@@ -4,5 +4,6 @@ from nillu import app
 
 DEBUG = True
 SECRET_KEY = '\xbb\xda`\x19\xefP"\xf5\xca\xcc\xe6\xee\r\xdd\x8e\x91\x8d\xf4\xa4\xc0\xdc\xad\x90\x7f'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(app.instance_path, 'nillu.db')
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(app.instance_path, 'nillu.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql://nilluuser:foobar@localhost/nilludb'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
