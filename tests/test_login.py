bc793a27b00403bc9393ca39ae8171b972a2c43b
Srinivasan Rangarajan <srinivasanr@gmail.com>
2017-04-15 11:06:20 +0530

    test incorrect password and incorrect email login separately

--- tests/test_login.py
+++ tests/test_login.py
@@ -9,11 +9,18 @@ class LoginTestCase(BaseTestCase):
         self.assertRedirects(rv, '/login?next=%2F')
 
     def test_correct_login(self):
+        """Test a correct email/password login."""
         rv = self.login('foo@example.com', 'foo')
         assert b'Logged in Successfully.' in rv.data
 
-    def test_incorrect_login(self):
+    def test_incorrect_password_login(self):
+        """Test an incorrect password login."""
         rv = self.login('foo@example.com', 'foo1')
+        assert b'Wrong email/password.' in rv.data
+
+    def test_incorrect_email_login(self):
+        """Test an incorrect email login."""
+        rv = self.login('spam@example.com', 'foo')
         assert b'Wrong email/password.' in rv.data
 
 if __name__ == '__main__':
