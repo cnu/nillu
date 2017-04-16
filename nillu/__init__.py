ba7cfc2a0d2cb31abc6efd7bce2c6299d597f5d7
Srinivasan Rangarajan <srinivasanr@gmail.com>
2017-04-17 00:45:00 +0530

    add a newline to br filter
    all new lines in the entries are converted to <br> and <p> tags
    fixes #33

--- nillu/__init__.py
+++ nillu/__init__.py
@@ -1,3 +1,6 @@
+import re
+
+from jinja2 import evalcontextfilter, Markup, escape
 from flask import Flask
 from flask_login import LoginManager
 
@@ -8,6 +11,18 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "warning"
+
+_paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
+
+
+@app.template_filter()
+@evalcontextfilter
+def nl2br(eval_ctx, value):
+    result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n') \
+        for p in _paragraph_re.split(escape(value)))
+    if eval_ctx.autoescape:
+        result = Markup(result)
+    return result
 
 
 import nillu.views
