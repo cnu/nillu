ad7e2e9f93cbce593887d69f72a5c7aa0f13fd93
Srinivasan Rangarajan <srinivasanr@gmail.com>
2017-04-20 00:32:50 +0530

    check if app is deployed on heroku and set config from env variables

--- instance/settings.py
+++ instance/settings.py
@@ -2,14 +2,22 @@ import os
 
 from nillu import app
 
-DEBUG = True
-SECRET_KEY = '\xbb\xda`\x19\xefP"\xf5\xca\xcc\xe6\xee\r\xdd\x8e\x91\x8d\xf4\xa4\xc0\xdc\xad\x90\x7f'
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(app.instance_path, 'nillu.db')
-SQLALCHEMY_DATABASE_URI = 'postgresql://nilluuser:foobar@localhost/nilludb'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+is_heroku = os.environ.get('IS_HEROKU', None)
 
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_USE_TLS = True
-MAIL_PORT = 587
-MAIL_USERNAME = 'username@gmail.com'
-MAIL_PASSWORD = 'password'
+if is_heroku:
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    if MAIL_USE_TLS.lower() == 'true':
+        MAIL_USE_TLS = True
+    else:
+        MAIL_USE_TLS = False
+    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+else:
+    DEBUG = True
+    SECRET_KEY = '\xbb\xda`\x19\xefP"\xf5\xca\xcc\xe6\xee\r\xdd\x8e\x91\x8d\xf4\xa4\xc0\xdc\xad\x90\x7f'
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(app.instance_path, 'nillu.db')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://nilluuser:foobar@localhost/nilludb'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
