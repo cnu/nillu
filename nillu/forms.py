dfb7465239fc57fe056df28467da81aec9d2f808
Srinivasan Rangarajan <srinivasanr@gmail.com>
2017-04-19 23:14:39 +0530

    Create UserAddForm

--- nillu/forms.py
+++ nillu/forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, AnyOf
 
 
 class LoginForm(FlaskForm):
     email = StringField('email', validators=[DataRequired(), Email()])
     password = PasswordField('password', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=True)
+
+
+class UserAddForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    role = StringField('role', validators=[DataRequired(), AnyOf(['developer', 'non-developer'])])
