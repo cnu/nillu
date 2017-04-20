b5699d1c0f3aa9f7ab8f46ebb0466c111e8b89b4
Srinivasan Rangarajan <srinivasanr@gmail.com>
2017-04-21 00:14:28 +0530

    Use markdown's nl2br extension
    and remove the custom nl2br template filter

--- nillu/__init__.py
+++ nillu/__init__.py
@@ -12,7 +12,7 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('settings.py')
 mail = Mail(app)
 heroku = Heroku(app)
-markdown = Markdown(app)
+markdown = Markdown(app, extensions=['nl2br'])
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -20,16 +20,6 @@ login_manager.login_view = "login"
 login_manager.login_message_category = "warning"
 
 _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
-
-
-@app.template_filter()
-@evalcontextfilter
-def nl2br(eval_ctx, value):
-    result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n') \
-        for p in _paragraph_re.split(escape(value)))
-    if eval_ctx.autoescape:
-        result = Markup(result)
-    return result
 
 
 @app.template_filter()
